5127c8b5ef516b3830c8be5032919941e5137324
Md Rakib Hossain <37966843+rakib10ms@users.noreply.github.com>
2022-02-27 00:09:27 +0600

    Add files via upload

---
+++ problem_solving/edabit/Day3/RemoveDuplicateArray.js
@@ -0,0 +1,11 @@
+// removeDups([1, 0, 1, 0]) ➞ [1, 0]
+
+// removeDups(["The", "big", "cat"]) ➞ ["The", "big", "cat"]
+
+// removeDups(["John", "Taylor", "John"]) ➞ ["John", "Taylor"]
+
+function removeDuplicate(items){
+  let uniqueArrays=[...new Set(items)]
+}
+removeDuplicate([1, 0, 1, 0])
+// removeDuplicate(["John", "Taylor", "John"])
